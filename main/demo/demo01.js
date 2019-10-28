1601fb1548cb15c8736a96d662e91ea6d9896013
ryan <1184518756@qq.com>
2019-10-28 18:38:30 +0800

    update for multi decorators

--- main/demo/demo01.js
+++ main/demo/demo01.js
@@ -2,35 +2,37 @@ import AnnotationGenerator from "../core/decorator-generator/AnnotationGenerator
 import {Autowired, Bean, BeanDescribe, Boot, Section, SectionDescribe} from "../core/decorator";
 import {EnergyWire} from "../core/decorator/EnergyWire";
 
-const LogCallMethod = AnnotationGenerator.generate(
-    class LogCallMethod extends SectionDescribe {
-        constructor() {
-            super();
-            this.params.before = ({origin}) => {
-                console.log(origin.name, 'called');
-            };
-            this.params.onError = ({resolve, error}) => {
-                resolve('Some thing went wrong, ' + error.message);
-            }
+class LogCallMethodDescribe extends SectionDescribe {
+    constructor() {
+        super();
+        this.params.before = ({origin}) => {
+            console.log(origin.name, 'called');
+        };
+        this.params.onError = ({resolve, error}) => {
+            resolve('Some thing went wrong, ' + error.message);
         }
     }
+}
+
+const LogCallMethod = AnnotationGenerator.generate(
+    LogCallMethodDescribe
 );
 
-const LoggedBean = AnnotationGenerator.generate(BeanDescribe,[LogCallMethod]);
+const LoggedBean = AnnotationGenerator.generate(BeanDescribe,[LogCallMethodDescribe]);
 
-// @Bean
-// class Configuration {
-//     port = 8081;
-//
-//     @Autowired({beanName: 'HelloWorld', isMapProperty: true})
-//     sayHello;
-//
-//     @Autowired({beanName: 'HelloWorld', isMapProperty: true})
-//     sayHi;
-//
-//     @EnergyWire('bootBean.port')
-//     syncPort;
-// }
+@Bean
+class Configuration {
+    port = 8081;
+
+    @Autowired({beanName: 'HelloWorld', isMapProperty: true})
+    sayHello;
+
+    @Autowired({beanName: 'HelloWorld', isMapProperty: true})
+    sayHi;
+
+    @EnergyWire('bootBean.port')
+    syncPort;
+}
 
 // @Bean
 // @LogCallMethod
@@ -46,21 +48,21 @@ class HelloWorld {
         return 'hi express-annotate!';
     }
 
-    // @Section({
-    //     after({lastValue}) {
-    //         return lastValue + 100
-    //     }
-    // })
+    @Section({
+        after({lastValue}) {
+            return lastValue + 100
+        }
+    })
     a = 1000;
 
-    // @Section({
-    //     onError({resolve, error}) {
-    //         console.log('thinking for error...');
-    //         if (.5 > Math.random()) {
-    //             resolve(`Get an error: [${error.message}], solution: dress more clothes.`)
-    //         }
-    //     }
-    // })
+    @Section({
+        onError({resolve, error}) {
+            console.log('thinking for error...');
+            if (.5 > Math.random()) {
+                resolve(`Get an error: [${error.message}], solution: dress more clothes.`)
+            }
+        }
+    })
     testError() {
         console.log(this.sayHi(), this.a);
         throw new Error('Weather is too cold!');
@@ -69,23 +71,23 @@ class HelloWorld {
 }
 
 
-// @Boot
-// class BootApplication {
-//
-//     @Autowired('Configuration')
-//     config;
-//
-//     @EnergyWire('Configuration')
-//     port;
-//
-//     @EnergyWire('HelloWorld')
-//     testError;
-//
-//     main() {
-//         console.log(this.config, this.port);
-//         console.log(this.config.sayHello());
-//         console.log(this.config.sayHi());
-//         console.log(this.config.syncPort);
-//         console.log(this.testError());
-//     }
-// }
+@Boot
+class BootApplication {
+
+    @Autowired('Configuration')
+    config;
+
+    @EnergyWire('Configuration')
+    port;
+
+    @EnergyWire('HelloWorld')
+    testError;
+
+    main() {
+        console.log(this.config, this.port);
+        console.log(this.config.sayHello());
+        console.log(this.config.sayHi());
+        console.log(this.config.syncPort);
+        console.log(this.testError());
+    }
+}
